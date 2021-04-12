c98d67d0af7cd5a1c9a21cac79f20cfdfa97df58
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-04-13 00:26:31 +0900

    modify: to be able to read file with fs module

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 // commanderモジュールをprogramとしてインポートする
 const program = require("commander");
+const fs = require("fs");
+
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
-
-// ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, (err, file) => {
+  console.log(file);
+});
