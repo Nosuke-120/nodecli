68bffdd53fd4302f9094559715840519b5c531ea
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-04-13 03:37:15 +0900

    modify: add gfm option

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 // commanderモジュールをprogramとしてインポートする
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
+
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする")
 
 // コマンドライン引数をcommanderでパースする
 program.parse(process.argv);
 const filePath = program.args[0];
 
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
+
 // ファイルを非同期で読み込む
-fs.readFile(filePath, (err, file) => {
-  console.log(file);
+fs.readFile(filePath, {encoding: "utf-8"}, (err, file) => {
+  if(err) {
+    console.log(err.message);
+    process.exit(1);
+    return;
+  }
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
 });
